fe6f50f2630b852507812e19a4741072ee744c76
Andrii Klymchuk (work) <qqvalarqq@gmail.com>
2024-04-03 15:27:21 +0300

    (test) fix hotline_ua urls and title regex

--- apps/common/constants.py
+++ apps/common/constants.py
@@ -7,7 +7,7 @@ TIME_FORMAT_DEFAULT = "%H:%M"
 
 ACTIVE_TITLE_CATEGORIES_DEFAULT = ["Побутова техніка", "Комп'ютери", "Смартфони"]
 
-TITLE_REGEX_DEFAULT = r"^[a-zA-Zа-яА-ЯєіїЄІЇ0-9\-'. ]{2,100}$"
+TITLE_REGEX_DEFAULT = r"^[a-zA-Zа-яА-ЯєіїЄІЇ0-9\-',. ]{2,100}$"
 
 RUN_VIP_CHECKER_INTERVAL_DEFAULT = 60 * 5
 RUN_ALL_CHECKER_INTERVAL_DEFAULT = 60 * 1
--- apps/hotline_ua/urls.py
+++ apps/hotline_ua/urls.py
@@ -7,7 +7,7 @@ app_name = "hotline_ua"
 urlpatterns = [
     path("", CheckerListView.as_view(), name="index"),
 
-    path("api/catalog/", api.CategoryCreateAPIView.as_view(), name="api-category"),
+    path("api/category/", api.CategoryCreateAPIView.as_view(), name="api-category"),
     path("api/filter/", api.FilterCreateAPIView.as_view(), name="api-filter"),
     path("api/checker/", api.CheckerCreateAPIView.as_view(), name="api-checker"),
     path("api/checker/<int:pk>/", api.HotlineUaCheckerUpdateAPIView.as_view(), name='api-update-checker'),
